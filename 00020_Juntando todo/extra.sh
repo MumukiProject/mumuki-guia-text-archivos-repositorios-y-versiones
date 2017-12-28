8c694fc08130e53c8b9a5c085100d33de3057e3b
franco@mumuki.org <franco@mumuki.org>
2017-12-28 08:47:40 -0500

    Mumuki Export on 2017-12-28 08:47:40 -0500

---
+++ 00020_Juntando todo/extra.sh
@@ -0,0 +1,31 @@
+mkdir curriculums
+cd curriculums
+cat <<HTML > curriculum-v1.txt
+<html>
+<head>
+  <title>Mi Currículum</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <header>
+    <h1>Mi Currículum</h1>
+  </header>
+  <main>
+    <section>
+      <h3>Habilidades</h3>
+      <ul>
+        <li>Programación con objetos</li>
+        <li>Ruby</li>
+        <li>HTML</li>
+        <li>Programación funcional</li>
+        <li>Haskell</li>
+      </ul>
+    </section>
+  </main>
+</body>
+</html>
+HTML
+sed 's:</ul>:<li>Gobstones</li></ul>' curriculum-v1.txt > curriculum-v1.txt
+sed 's:</ul>:<li>SQL</li></ul>' curriculum-v2.txt > curriculum-v3.txt
+sed 's:</ul>:<li>Bases de datos</li></ul>' curriculum-v3.txt > curriculum-v4.txt
+cd
--- 00020_Juntando todo/hint.md
+++ 00020_Juntando todo/hint.md
@@ -2,4 +2,4 @@ Si no sabés cómo arrancar, te proponemos:
 
 1. Dirigite al directorio `curriculums` usando `cd`;
 2. Buscá la versión más nueva con ayuda de `ls`
-3. Editá este archivo utilizando `sed`. Por ejemplo, podrías reemplazar el `</ul>` final por un `<li>` más un `</ul>`
+3. Editá este archivo utilizando `sed`. Por ejemplo, podrías reemplazar el `</ul>` final por un `<li>`, la nueva habilidad, y un  `</li></ul>`
